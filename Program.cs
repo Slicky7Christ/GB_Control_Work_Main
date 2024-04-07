8dec955aaa1746fb126665372aedce87ad53a454
Kirill Vasilev <vasilev98k@mail.ru>
2024-04-07 20:30:20 +0700

    Added examples, array initialization, output function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Примеры
+{"Hello", "2", "world", ":-)"}
+{"1234", "1567", "-2", "computer science"}
+{"Russia", "Denmark”, "Kazan"}
+*/
+
+string[] starting_Array = { "Hello", "2", "world", ":-)" };
+string[] final_Array;
+PrintArray(starting_Array);
+
+string[] PrintArray(string[] print_Array)
+{
+    int length_arr = 0;
+    foreach (string i in print_Array)
+    {
+        Console.Write(i);
+        if (length_arr != print_Array.Length - 1)
+        {
+            Console.Write(", ");
+            length_arr++;
+        }
+    }
+    return print_Array;
+}
